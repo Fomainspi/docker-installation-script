d43d11d15f56d2f31b0d7ac02f9a5a8597d7ce87
Ubuntu <ubuntu@ip-172-31-13-110.ap-southeast-2.compute.internal>
2025-09-08 06:44:14 +0000

    first commit

---
+++ install-docker.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install the Docker packages.
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Verify installation
+docker --version
+
+echo "✅ Docker installed successfully!"
+
+
+# Allow Running Docker Without sudo
+# Create the docker group (if not already created):
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+#Exit the server and log in again
+echo "please exit the server and log in again"
